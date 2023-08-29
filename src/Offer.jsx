067e2fe28dd2bf67873ebfa10a3aab0888ce9d08
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-28 21:21:42 -0400

    Add Offer Component

---
+++ src/Offer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Offer = () => {
+  return (
+    <>
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="offerings">
+        <div>
+          Offerings Go Here
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Offer;
