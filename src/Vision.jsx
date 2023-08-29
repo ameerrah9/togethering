4ed3acd5d3b7d3c2f5ece87dd292b996bda76f21
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-28 21:22:00 -0400

    Add Vision Component & Logo

---
+++ src/Vision.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Vision = () => {
+  return (
+    <>
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="vision">
+        <div>
+          Vision Goes Here
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Vision;
