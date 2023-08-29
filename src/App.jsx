14fcfddf363c27b48e91f69bcbef488856c4b804
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-28 21:21:04 -0400

    Add App Root Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import NavBar from './NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
