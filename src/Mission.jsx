9aa536ba27ae6ee50d955315014e37900ff1e7c0
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-28 21:21:24 -0400

    Add Mission Component

---
+++ src/Mission.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Mission = () => {
+  return (
+    <>
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="mission">
+        <div>
+          Mission Goes Here
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Mission;
