cb812e55240bd8b3980154d504405853d92a06c6
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-28 21:21:14 -0400

    Add Contact Component

---
+++ src/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="contact">
+        <div>
+          Contact Goes Here
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Contact;
