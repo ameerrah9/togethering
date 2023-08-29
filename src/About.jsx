871724f6010f8e463be26aa6827ed05a8293bc53
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-28 21:20:38 -0400

    Add About Page

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div>
+          About Goes Here
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default About;
